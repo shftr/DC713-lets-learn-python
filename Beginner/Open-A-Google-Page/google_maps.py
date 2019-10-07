84c0cbe7d7b232e0a08bbc10f5a6ddb650013a1e
shftr <sh_ftr@protonmail.ch>
2019-10-07 13:04:54 -0500

    Added Beginner/Open-A-Google-Page

---
+++ Beginner/Open-A-Google-Page/google_maps.py
@@ -0,0 +1,14 @@
+#!/usr/bin/local/python3
+
+# you can use help($libraryname) in python REPL to look at syntax of a library or help($libraryname.functioncall) for a
+# function.
+
+#example: help(sys.argv) in REPL
+# google is your friend
+
+import sys
+import webbrowser # library to open a web page
+
+page = 'https://google.com/maps/place/'
+
+
